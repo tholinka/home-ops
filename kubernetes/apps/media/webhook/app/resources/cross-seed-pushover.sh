af468e1912a12fc80321dc0c1f8487e309e18e87
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2025-04-05 19:31:21 -0500

    update webhooks

--- kubernetes/apps/media/webhook/app/resources/cross-seed-pushover.sh
+++ kubernetes/apps/media/webhook/app/resources/cross-seed-pushover.sh
@@ -22,16 +22,23 @@ function notify() {
 
 	if [[ "${event}" == "RESULTS" && "${result}" == "INJECTED" ]]; then
 		printf -v pushover_title "Cross-Seed Injection"
-		printf -v pushover_msg "<b>%s</b><small>\nFrom %s to %s</small>\n\n<b>Source:</b> %s</small>" \
+		printf -v pushover_msg "<b>%s</b><small>\nCategory: %s</small><p>\nPartial Match (Paused): %s</p><small>\nFrom %s to %s</small>\n\n<b>Source:</b> %s</small>" \
 			"$(_jq '.extra.name')" \
+			"$(_jq '.extra.category')" \
+			"$(_jq '.extra.paused')" \
 			"$(_jq '.extra.searchee.trackers[0]')" \
 			"$(_jq '.extra.trackers[0]')" \
 			"$(_jq '.extra.source')"
-		printf -v pushover_priority "%s" "low"
+
+		printf -v pushover_url "https://qb.${SECRET_DOMAIN}/#/torrent/%s" "$(_jq '.extra.infoHashes')"
+		printf -v pushover_url_title "View in qbit"
+
+		printf -v pushover_priority "%s" \
+			"$([[ $(_jq '.extra.paused') == 'true' ]] && echo "emergency" || echo "low")"
 	fi
 
 	apprise -vv --title "${pushover_title}" --body "${pushover_msg}" \
-		"${PUSHOVER_URL}?priority=${pushover_priority}&format=markdown"
+		"${PUSHOVER_URL}?url=${pushover_url}&url_title=${pushover_url_title}&priority=${pushover_priority}&format=markdown"
 }
 
 function main() {
