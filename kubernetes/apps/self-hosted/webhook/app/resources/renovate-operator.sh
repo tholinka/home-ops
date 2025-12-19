bcba6ed7e749ed7a0e1a5d1f202b0146306423c2
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2025-12-18 21:09:40 -0600

    chore(renovate): yoink webhook

---
+++ kubernetes/apps/self-hosted/webhook/app/resources/renovate-operator.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Incoming arguments
+JOB=${1:-}
+NAMESPACE=${2:-}
+PROJECT=${3:-}
+
+# URL encode the project name
+PROJECT=$(echo "${PROJECT}" | jq -Rr @uri)
+
+# in github webhooks, enable: pull requests and issues
+
+curl -s -X POST \
+  "http://renovate-operator-webhook.renovate.svc.cluster.local:8082/webhook/v1/schedule?job=${JOB}&namespace=${NAMESPACE}&project=${PROJECT}"
