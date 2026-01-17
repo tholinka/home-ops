9b225081223f76e962b39747b660feb655a4e2bc
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2026-01-16 22:32:54 -0600

    chore(renovate): renovate-operator-webhook service no longer exists

--- kubernetes/apps/self-hosted/webhook/app/resources/renovate-operator.sh
+++ kubernetes/apps/self-hosted/webhook/app/resources/renovate-operator.sh
@@ -12,4 +12,4 @@ PROJECT=$(echo "${PROJECT}" | jq -Rr @uri)
 # in github webhooks, enable: pull requests and issues
 
 curl -s -X POST \
-  "http://renovate-operator-webhook.renovate.svc.cluster.local:8082/webhook/v1/schedule?job=${JOB}&namespace=${NAMESPACE}&project=${PROJECT}"
+  "http://renovate-operator.renovate.svc.cluster.local:8082/webhook/v1/schedule?job=${JOB}&namespace=${NAMESPACE}&project=${PROJECT}"
