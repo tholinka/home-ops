f07280fb3c7833f6658309774409741b1c954aae
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2026-04-28 20:27:19 -0500

    chore(renovate): fix renovate webhook

--- kubernetes/apps/self-hosted/webhook/app/resources/renovate-operator.sh
+++ kubernetes/apps/self-hosted/webhook/app/resources/renovate-operator.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# this basically just directly forwards the events
+# this allows using the renovate-operator's github webhook support,
+# without having to expose the operator as external
+
 # Incoming arguments
 JOB=${1:-}
 NAMESPACE=${2:-}
-PROJECT=${3:-}
-
-# URL encode the project name
-PROJECT=$(echo "${PROJECT}" | jq -Rr @uri)
+BODY=${3:-}
 
 # in github webhooks, enable: pull requests and issues
-
 curl -s -X POST \
-  "http://renovate-operator.renovate.svc.cluster.local:8082/webhook/v1/schedule?job=${JOB}&namespace=${NAMESPACE}&project=${PROJECT}"
+  "http://renovate-operator.renovate.svc.cluster.local:8082/webhook/v1/github?job=${JOB}&namespace=${NAMESPACE}" \
+  -H "Content-Type: application/json" \
+  --data-raw "${BODY}"
